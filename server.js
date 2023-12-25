343ac1653edd93be292fea3f1395ddf81c0e1544
Ashish Kumar <83261616+Ashish9140@users.noreply.github.com>
2023-12-25 14:00:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,19 @@
 const express = require('express');
 const http = require('http');
 const Socket = require('./socket');
-const cors= require("cors");
+const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send("Hello From Backend");
+  res.json({ message: "Hello From Backend" })
 });
 
 // socket connection
-const io = require('socket.io')(server);
-Socket(io);
+// const io = require('socket.io')(server);
+// Socket(io);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
