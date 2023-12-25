17aacf4aaa7d1c08911513fa2d53bc3eb96b49aa
Ashish Kumar <83261616+Ashish9140@users.noreply.github.com>
2023-12-25 14:05:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/', (req, res) => {
 });
 
 // socket connection
-// const io = require('socket.io')(server);
-// Socket(io);
+const io = require('socket.io')(server);
+Socket(io);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
